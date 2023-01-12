a4deb1dbac050830c3e2a5fa7d5dfe8ba892515c
lord <alair8676@gmail.com>
2023-01-12 16:13:18 -0300

    organizando pacotes

--- anotomia-de-classes/src/boletinEstudantil.java
+++ anotomia-de-classes/src/boletinEstudantil.java
@@ -1,12 +1,12 @@
 public class boletinEstudantil {
-        public static void main(String[] args) {
-        int mediaFinal = 6;
-        if(mediaFinal<6)	
-        System.out.println("REPROVADO"); 
-        else if(mediaFinal==6)
-        System.out.println("PROVA MINERVA"); 
-        else
-        System.out.println("APROVADO"); 		
-        }
+    public static void main(String[] args) {
+		int mediaFinal = 8;
+		if (mediaFinal < 6)
+			System.out.println("REPROVADO");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");
+	}
         }
 
