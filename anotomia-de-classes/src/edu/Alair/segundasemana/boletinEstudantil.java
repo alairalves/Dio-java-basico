9cb7e983c79390578a80c75c9e14f8ec7a330a8a
lord <alair8676@gmail.com>
2023-01-12 16:41:54 -0300

    pacotes organizados dentro da pasta src

---
+++ anotomia-de-classes/src/edu/Alair/segundasemana/boletinEstudantil.java
@@ -0,0 +1,14 @@
+package edu.alair.segundasemana;
+
+public class boletinEstudantil {
+    public static void main(String[] args) {
+		int mediaFinal = 7;
+		if (mediaFinal < 6)
+			System.out.println("REPROVADO");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");
+	}
+}
+
