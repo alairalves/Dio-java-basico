113868e3e03cfa6fc7c69b8d98e1db45a71e2890
lord <alair8676@gmail.com>
2023-01-12 17:34:28 -0300

    #

--- anotomia-de-classes/src/edu/Alair/terceirasemana/tiposEvariaveis/src/tipoEvariaveis.java
+++ anotomia-de-classes/src/edu/Alair/terceirasemana/tiposEvariaveis/src/tipoEvariaveis.java
@@ -1,5 +1,9 @@
 public class tipoEvariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+
+        final int NUMERO = 5;
+        
+        System.out.print(NUMERO);
     }
 }
