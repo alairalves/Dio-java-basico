2e838c7437f68b7a060d63d685a858b432d18605
lord <alair8676@gmail.com>
2023-02-02 01:32:11 -0300

    02-02-2023

---
+++ anotomia-de-classes/src/edu/Alair/terceirasemana/tiposEvariaveis/src/operadores.java
@@ -0,0 +1,28 @@
+public class operadores {
+    public static void main(String[] args) {
+      String nomeCompleto = "Alair" + "Alves";
+      System.out.println(nomeCompleto);
+
+      String concatenacao ="?"; 
+
+concatenacao = 1+1+1+"1";
+
+System.out.println(concatenacao);
+
+concatenacao = 1+"1"+1+1;
+
+System.out.println(concatenacao);
+
+concatenacao = 1+"1"+1+"1";
+
+System.out.println(concatenacao);
+
+concatenacao = "1"+1+1+1;
+
+System.out.println(concatenacao);
+
+concatenacao = "1"+(1+1+1);
+
+System.out.println(concatenacao);
+    }
+}
