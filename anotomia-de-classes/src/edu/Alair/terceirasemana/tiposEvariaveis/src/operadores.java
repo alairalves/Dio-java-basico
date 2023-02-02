7d201a53e0f3de8ba9b17aca04c32ce77a902e66
lord <alair8676@gmail.com>
2023-02-02 02:30:02 -0300

    .

--- anotomia-de-classes/src/edu/Alair/terceirasemana/tiposEvariaveis/src/operadores.java
+++ anotomia-de-classes/src/edu/Alair/terceirasemana/tiposEvariaveis/src/operadores.java
@@ -9,13 +9,42 @@ public class operadores {
 
      // System.out.println(++ numero);
       // System.out.println(numero);
-     boolean variavel = true;
 
-     System.out.println(!variavel);
-     System.out.println(variavel);
 
-     variavel =!variavel;
 
-     System.out.println(variavel);
+     //boolean variavel = true;
+
+     //System.out.println(!variavel);
+     //System.out.println(variavel);
+
+     //variavel =!variavel;
+
+     //System.out.println(variavel);
+
+     // classe Operadores.java
+int a, b;
+
+a = 5;
+b = 5;
+
+String resultado = a==b ? "verdadeiro" : "falso";
+/*String resultado = "";
+
+if(a==b){
+    resultado = "Verdadeiro";
+}else{
+    resultado = "falso";
+}
+
+
+
+ EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+if(a==b)
+   resultado = "verdadeiro";
+else
+   resultado = "falso";
+*/
+
+System.out.println(resultado);
 }
 }
