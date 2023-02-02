89ef5788df9afceafa66e35e68279f42f365a78e
lord <alair8676@gmail.com>
2023-02-02 01:51:53 -0300

    .

--- anotomia-de-classes/src/edu/Alair/terceirasemana/tiposEvariaveis/src/operadores.java
+++ anotomia-de-classes/src/edu/Alair/terceirasemana/tiposEvariaveis/src/operadores.java
@@ -1,10 +1,21 @@
 public class operadores {
     public static void main(String[] args) {
       String nomeCompleto = "Alair" + " Alves";
-      System.out.println(nomeCompleto);
+     // System.out.println(nomeCompleto);
 
-      int numero = 5;
+      //int numero = 5;
 
-      System.out.println(- numero);
+      //System.out.println(- numero);
+
+     // System.out.println(++ numero);
+      // System.out.println(numero);
+     boolean variavel = true;
+
+     System.out.println(!variavel);
+     System.out.println(variavel);
+
+     variavel =!variavel;
+
+     System.out.println(variavel);
 }
 }
